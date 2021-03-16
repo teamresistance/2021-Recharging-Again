2b9f83cbe0165bd878d8a7a3672b47f48d33a1b2
JimHofm52 <JimHofmann52@gmail.com>
2021-03-16 15:08:19 -0400

    Switch to diff only.

--- src/main/java/frc/robot/auto/functions/CurveTurn.java
+++ src/main/java/frc/robot/auto/functions/CurveTurn.java
@@ -11,12 +11,11 @@ import frc.io.hdw_io.IO;
 
 public class CurveTurn extends AutoFunction {
 
-    // private WPI_TalonSRX right = IO.drvMasterTSRX_R; // right motor
-    // private WPI_TalonSRX left = IO.drvMasterTSRX_L; // left motor
-    // private WPI_VictorSPX rightSlave = IO.drvFollowerVSPX_R;
-    // private WPI_VictorSPX leftSlave = IO.drvFollowerVSPX_L;
-    // private DifferentialDrive diffDrv = new DifferentialDrive(left, right);
-    private DifferentialDrive diffDrv = IO.diffDrv_M;
+    private WPI_TalonSRX right = IO.drvMasterTSRX_R; // right motor
+    private WPI_TalonSRX left = IO.drvMasterTSRX_L; // left motor
+    private WPI_VictorSPX rightSlave = IO.drvFollowerVSPX_R;
+    private WPI_VictorSPX leftSlave = IO.drvFollowerVSPX_L;
+    private DifferentialDrive diffDrv = new DifferentialDrive(left, right);
 
     private int state;
     private int prvState;
@@ -36,10 +35,8 @@ public class CurveTurn extends AutoFunction {
 
     public void init() {
         finished = false;
-        diffDrv.curvatureDrive(0.0, 0.0, false);
-        IO.follow();
-        // left.set(0);
-        // right.set(0);
+        left.set(0);
+        right.set(0);
         state = -1;
         prvState = 0;
     }
@@ -65,15 +62,14 @@ public class CurveTurn extends AutoFunction {
     public void done() {
         finished = true;
         diffDrv.tankDrive(0, 0);
-        // left.set(ControlMode.Disabled, 0);
-        // right.set(ControlMode.Disabled, 0);
+        left.set(ControlMode.Disabled, 0);
+        right.set(ControlMode.Disabled, 0);
     }
 
     public void update() {
         //sdbUpdate();
-        IO.follow();
-        // leftSlave.follow(left);
-        // rightSlave.follow(right);
+        leftSlave.follow(left);
+        rightSlave.follow(right);
     }
     
 }
