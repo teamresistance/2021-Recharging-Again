799e95b72e706d2e076100a0d323433e236df004
JimHofm52 <JimHofmann52@gmail.com>
2021-03-16 15:03:50 -0400

    Chged code to revolve 1 ping only.  AND COMMENTS.

--- src/main/java/frc/robot/Subsystem/Revolver.java
+++ src/main/java/frc/robot/Subsystem/Revolver.java
@@ -7,18 +7,10 @@ until requested to deliever them to the shooter.
 */
 package frc.robot.Subsystem;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.IMotorController;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.io.hdw_io.IO;
 import frc.io.hdw_io.InvertibleDigitalInput;
-import frc.io.hdw_io.InvertibleSolenoid;
 import frc.io.joysticks.JS_IO;
 import frc.util.Timer;
 
@@ -26,22 +18,21 @@ import frc.util.Timer;
 //          BANNER SENSOR ON FEED NOT ON SUCK 
 
 public class Revolver {
-    private static Victor revolver = IO.revolverRot;
-    private static InvertibleDigitalInput atOneRevolution = IO.revolerIndexer; // mag
-    private static InvertibleDigitalInput ballInSnorfler = IO.snorfHasBall;
-    private static InvertibleDigitalInput nextSpaceOpen = IO.revolNextSpaceOpen;
+    private static Victor revolver = IO.revolverRot;        //PWM ESC control
+    private static InvertibleDigitalInput atIndexStop = IO.revolerIndexer;  //Mag switch on gear for one rotation
+    private static InvertibleDigitalInput ballInSnorfler = IO.snorfHasBall;     //Ball at top of snorfler
+    private static InvertibleDigitalInput nextSpaceOpen = IO.revolNextSpaceOpen;//Banner snsr, chk if a ball is in rcv'iing slot
 
     public static int state = 0;
     private static boolean locked = false;
-    private static int ballCnt = 0; // Increment when a ball is loaded. Clear on unload.
-    private static boolean isFull = false;
-    private static boolean jammedBall = false; // Jammed ball, attempt to clear by reversing
-    private static boolean hasUnloaded;
-    private static boolean hasShot;
-    private static int slowFireCnt = 0;
+    private static int ballCnt = 0;             // Increment when a ball is loaded. Clear on unload.
+    private static boolean isFull = false;      //Ball cnt > 4
+    private static boolean jammedBall = false;  //Jammed ball, attempt to clear by reversing
+    private static boolean hasUnloaded;         //Ball cnt < 1?
+    private static boolean hasShot;             //??
+    private static int slowFireCnt = 0;         //??
 
-    private static double loadPct = 0.15; // Spd when loading
-    private static double unloadPct = 0.25; // Spd when unloading
+    private static double revPct = 0.50;        // Spd when Indexing
 
     private static Timer delayTimer = new Timer(0.5);
 
@@ -54,27 +45,20 @@ public class Revolver {
     }
 
     public static void determ() { // determinator of state
-            if (JS_IO.btnFireShooter.isDown())
-                state = 11; // Unload
+        if (JS_IO.btnFireShooter.isDown()) state = 11; // Unload
 
         //single fire test
-        if (JS_IO.btnSlowFire.onButtonPressed()) {
-            state = 25;
+        if (JS_IO.btnSlowFire.onButtonPressed()) state = 25;
             //slowFireCnt = 0;
-        }
 
-        if (JS_IO.btnStop.isDown())
-            state = 0;
+        if (JS_IO.btnStop.isDown()) state = 0;
 
         if (JS_IO.btnIndex.isDown()) {
             ballCnt = 0;
             state = 30;
         }
 
-        if (Snorfler.hasBall() && state != 1 && !isFull) {
-            state = 1; // Load
-        }
-
+        if (Snorfler.hasBall() && state != 1 && !isFull) state = 1; // Load
 
         if (IO.revolver_HAA && state < 90) { // jammed Ball
             state = 90; // Clear jammed ball
@@ -88,224 +72,54 @@ public class Revolver {
         switch (state) {
             case 0: // everything off, revolverSpeed. Waiting for Load or Unload.
                 cmdUpdate(0.0);
+                if (delayTimer.hasExpired(0.05, state)) state++; // Initialize timer for covTrgr
                 break;
-            // ------------ Loading --------------------
-            case 1: // waiting for intake, snorfling
+            // ------------ Start indexing --------------------
+            case 1: // wait some time ... if needed
                 cmdUpdate(0.0);
-                if (delayTimer.hasExpired(0.5, state))
-                    state++; // Settling time
+                if (delayTimer.hasExpired(0.05, state)) state++; // Wait time (settle time?)
                 break;
-            case 2: // indexing and rotating
-                cmdUpdate(loadPct);
+            case 2: // Motor on
+                cmdUpdate(revPct);
+                if (!atIndexStop.get()) state++;    // Wait for sw goes false.
+                break;
+            case 3: // Wait for sw to go true
+                cmdUpdate(revPct);
+                if(atIndexStop.get()) state++;      //Wait until sw goes true again
+                break;
+            case 4: // Brake with small negative
+                cmdUpdate(-0.15);
+                if (delayTimer.hasExpired(0.07, state)) state++;    //Brake time
+                break;
+            case 5: //Stop
+                cmdUpdate(0.0);
+                if (delayTimer.hasExpired(0.15, state)) state++;    //Shutdown time
+                break;
+            case 6: //return to start
+                state = !isFull() && !nextSpaceOpen.get() ? 0 : 1;
+                break;
+
+
+            // --------------- Jammed Ball Safety ----------------
+            case 90: // Safety everything off.
+                cmdUpdate(0.0);
+                IO.revolver_HAA = false;
                 state++;
                 break;
-            case 3: // Wait for index sw to clear then start checking. When seen again stop.
-                cmdUpdate(loadPct);
-                if (delayTimer.hasExpired(0.25, state) && atOneRevolution.get()) {
-                    cmdUpdate(0.0);
-                    state = 19;
-                }
+            case 91: // Wait momentarily before reversing.
+                if (delayTimer.hasExpired(0.2, state)) state++;
                 break;
-            case 19: // settling time????
-                cmdUpdate(0);
-                if (delayTimer.hasExpired(0.1, state) && !atOneRevolution.get()) {
-                    state = 4;
-                }
+            case 92: // Reverse momentarily.
+                cmdUpdate(-revPct);
+                if (delayTimer.hasExpired(0.3, state)) state++;
                 break;
-            case 4: // Done goto state 0
-                cmdUpdate(0);
-                if (delayTimer.hasExpired(0.25, state)) {
-                    if (nextSpaceOpen.get()) {
-                        state = 0;
-                    } else {
-                        state = 5;
-                    }
-                }
-                break;
-            case 5: // check if the next slot is empty
-                cmdUpdate(loadPct);
-                if (delayTimer.hasExpired(0.25, state) && atOneRevolution.get()) {
-                    state = 6;
-                }
-                break;
-            case 6:
-                cmdUpdate(0);
-                if (delayTimer.hasExpired(.1, state)) {
-                    if (!nextSpaceOpen.get()) {
-                        isFull = true;
-                    }
-                    state = 0;
-
-                }
-
-                break;
-
-            // ------------ Unloading -------------
-            case 11: // check if can unload
+            case 94: // Stop momentarily
                 cmdUpdate(0.0);
-                // Turret.isOnTarget() &&
-                if (Shooter.isAtSpeed() && Injector.isRunning()) { // if at speed and on target
-                    state++;
-                    isFull = false;
-                }
+                if (delayTimer.hasExpired(0.2, state)) state++;
                 break;
-            case 12: // start unloading (revolver)
-                cmdUpdate(unloadPct);
-                if (delayTimer.hasExpired(3.0, state)) { // waiting for all balls
-                    hasUnloaded = true;
-                    state++;
-                }
-                break;
-            case 13: // move into loading position
-                cmdUpdate(loadPct);
-                if (atOneRevolution.get()) {
-                    hasUnloaded = false;
-                    state = 0;
-                }
-                break;
-
-            // // ------------ Slow Unloading --------------
-            // // case 14:
-            // // cmdUpdate(0.0);
-            // // // Turret.isOnTarget() &&
-            // // if (Shooter.isAtSpeed() && Injector.isRunning()) { // if at speed and on
-            // // target
-            // // state++;
-            // // isFull = false;
-            // // }
-            // // break;
-            // // case 15:
-            // // cmdUpdate(.6 * unloadPct);
-            // // if (delayTimer.hasExpired(3.5, state)) { // waiting for all balls
-            // // hasUnloaded = true;
-            // // state++;
-            // // }
-            // // break;
-            // // case 16:
-            // // cmdUpdate(loadPct);
-            // // if (atOneRevolution.get()) {
-            // // hasUnloaded = false;
-            // // state = 0;
-            // // }
-            // // break;
-
-            // // slow fire 2
-            // case 14:
-            // cmdUpdate(0.0);
-            // // Turret.isOnTarget() &&
-            // if (Shooter.isAtSpeed() && Injector.isRunning()) { // if at speed and on
-            // target
-            // isFull = false;
-            // state++;
-            // }
-            // break;
-            // case 15:
-            // cmdUpdate(0.0);
-            // if (slowFireCnt == 5) {
-            // hasUnloaded = true;
-            // state = 18;
-            // } else {
-            // state = 16;
-            // }
-            // break;
-            // case 16:
-            // cmdUpdate(unloadPct);
-            // slowFireCnt++;
-            // state++;
-            // break;
-            // case 17:
-            // cmdUpdate(unloadPct);
-            // if (delayTimer.hasExpired(0.25, state) && atOneRevolution.get()) {
-            // cmdUpdate(0.0);
-            // state = 15;
-            // }
-            // break;
-            // case 18:
-            // cmdUpdate(loadPct);
-            // if (atOneRevolution.get()) {
-            // hasUnloaded = false;
-            // state = 0;
-            // }
-            // break;
-
-            // ------------ Single Fire -------------------
-            case 25:
-                if (Shooter.isAtSpeed() && Injector.isRunning()) {
-                    state++;
-                } // end at state 4??
-                break;
-            case 26:
-                cmdUpdate(unloadPct);
-                if (atOneRevolution.get()) {
-                    hasShot = true;
-                    state++;
-                }
-            case 27:
-                cmdUpdate(loadPct);
-                if (atOneRevolution.get() && delayTimer.hasExpired(.25, state)) {
-                    hasShot = false;
-                    if (isFull()) {
-                        isFull = false;
-                        state = 30;
-                    } else {
-                        state = 30;
-                    }
-                }
-                break;
-            // ------------ Reindex for missed slot -------------------
-            case 30: // reindex, Check for open slot
-                cmdUpdate(0.0);
-                if (nextSpaceOpen.get()) { // if there is NO ball in the next space
-                    isFull = false;
-                    state = 0;
-                } else {
-                    if (ballCnt == 5) {
-                        isFull = true;
-                        state = 0;
-                    } else {
-                        state = 31;
-                    }
-                }
-                break;
-            case 31: //
-                cmdUpdate(loadPct);
-                ballCnt++;
-                state = 32;
-                break;
-            case 32: // Wait for index sw to clear then start checking. When seen again stop.
-                cmdUpdate(loadPct);
-                if (delayTimer.hasExpired(0.25, state) && atOneRevolution.get()) {
-                    cmdUpdate(0.0);
-                    state = 33;
-                }
-                break;
-            case 33:
-                cmdUpdate(0);
-                if (delayTimer.hasExpired(0.1, state)) {
-                    cmdUpdate(0.0);
-                    state = 30;
-                }
-                break;
-            // --------------- Jammed Ball Safety ----------------
-            case 90: // Safety everything off. Wait momentarily before reversing.
-                cmdUpdate(0.0);
-                if (delayTimer.hasExpired(0.2, state)) {
-                    IO.revolver_HAA = false;
-                    state++;
-                }
-                break;
-            case 91: // Reverse momentarily before trying again.
-
-                cmdUpdate(-loadPct);
-                if (delayTimer.hasExpired(0.5, state))
-                    state++;
-                break;
-            case 92: // Stop momentarily before trying again.
-                cmdUpdate(0.0);
-                if (delayTimer.hasExpired(0.2, state)) {
-                    jammedBall = false;
-                    state = 3;
-                }
+            case 96: // before trying to index again.
+                jammedBall = false;
+                state = 1;
                 break;
 
             default: // everything off
@@ -316,46 +130,44 @@ public class Revolver {
 
     }
 
-    // Stuff Updates
+    /**Update stuff */
     private static void stfUpdate() {
         if (IO.revolver_HAA)
             jammedBall = true; // Try to clear by reversing
         // isFull = (ballCnt >= 5) || !nextSpaceOpen.get();
     }
 
-    // turning off and on certain functions
+    /**Send revolver speed cmd to motor.  Check for motor hi alarm, jam, and stop motor. */
     private static void cmdUpdate(double revolverSpeed) {
-        revolver.set((!IO.revolver_HAA) ? revolverSpeed : 0); // HAA now, ballJammed in state
+        //Jammed ball will not break anything "critical" so handle in state machine.
+        revolver.set(revolverSpeed);
+        // revolver.set((!IO.revolver_HAA) ? revolverSpeed : 0); // HAA now, ballJammed in state
     }
 
-    public static boolean isFull() {
-        return isFull;
-    }
+    /**@return true if revolver is at index, has an open slot and isn't indexing. */
+    public static boolean rdy2Rcv() { return atIndexStop.get() && nextSpaceOpen.get() /*&& !indexing*/; }
 
-    // return the state of the Revolver
-    public static int getState() {
-        return state;
-    }
+    /**@return true if revolver thinks it has 5 balls. */
+    public static boolean isFull() { return ballCnt > 5; }
 
-    public static boolean hasUnloaded() {
-        return hasUnloaded;
-    }
+    /**@return state of the Revolver.  State assignments may chg, use statuses. */
+    public static int getState() { return state; }
 
-    public static boolean hasShot() {
-        return hasShot;
-    }
+    /**@return Has unloaded all balls rotated for 3 secs? */
+    public static boolean hasUnloaded() { return ballCnt < 1; }
 
-    // SmartDashboard Updates
-    private static void sdbInit() {
+    /**Has shot? */
+    public static boolean hasShot() { return hasShot; }     //No i deer.
 
-    }
+    /**Initialize sdb for revolver */
+    private static void sdbInit() { }
 
-    // SmartDashboard Updates
+    /**SmartDashboard Updates */ 
     private static void sdbUpdate() {
         SmartDashboard.putBoolean("Rev HAA", IO.revolver_HAA);
         SmartDashboard.putNumber("Revolver State", state);
         SmartDashboard.putBoolean("Rev BallInSnorf", nextSpaceOpen.get());
-        SmartDashboard.putBoolean("atOneRev", atOneRevolution.get());
+        SmartDashboard.putBoolean("atIndexStop", atIndexStop.get());
         SmartDashboard.putNumber("slow count", slowFireCnt);
     }
 
