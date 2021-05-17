3ebd476903a26b59083dd70f70ebbb8d7d0a5870
JimHofm52 <JimHofmann52@gmail.com>
2021-05-17 17:05:40 -0400

    Cleanup & add comments.

--- src/main/java/frc/io/hdw_io/InvertibleDigitalInput.java
+++ src/main/java/frc/io/hdw_io/InvertibleDigitalInput.java
@@ -5,37 +5,53 @@ History:
 JCH - 11/8/2019 - added additional functions.  isActive/Deactive & onAactive/Deactive
 S - 11/8/2019 - Original Release
 
-TODO: - Check it out.
-
 Desc:
 Sets the feedback from a digital input to normally closed.
 */
 
 import edu.wpi.first.wpilibj.DigitalInput;
 
+/**
+ * Creates a DigitalIput that can return an invarted status.
+ */
 public class InvertibleDigitalInput {
     private DigitalInput limitSwitch;
     private boolean isInverted;
     private boolean previousState;
 
-    //Makes Object and sets it to either inverted or not
+    /**Construct for an Object and sets it to either inverted or normal. */
     public InvertibleDigitalInput(int channel, boolean invert) {
         isInverted = invert;
         limitSwitch = new DigitalInput(channel);
     }
-    //Returns the state of the object
+
+    /**
+     * @return the current state.  If isInverted then inverted state.
+     */
     public boolean get() {
-        return (isInverted ? !limitSwitch.get() : limitSwitch.get());
+        return (isInverted ^ limitSwitch.get());
+        // return (isInverted ? !limitSwitch.get() : limitSwitch.get());
     }
 
-    public boolean isActive(boolean currentState) {
+    /**
+     * @return true if current state is false.
+     */
+    public boolean isActive() {
+    // public boolean isActive(boolean currentState) {
         return get();
     }	
-	
-	public boolean isDeactive(boolean currentState) {
+    
+    /**
+     * @return true if current state is false.
+     */
+    public boolean isDeactive() {
+	// public boolean isDeactive(boolean currentState) {
 		return !get();
 	}	
 	
+    /**
+     * @return true if state has changed to true.
+    */
 	public boolean onActive() {
         if(get() != previousState){
             previousState =  get();
@@ -44,7 +60,10 @@ public class InvertibleDigitalInput {
             return false;
         }
 	}
-	
+    
+    /**
+     * @return true if state has changed to false.
+     */
 	public boolean onDeactive() {
         if(!get() != previousState){
             previousState =  get();
