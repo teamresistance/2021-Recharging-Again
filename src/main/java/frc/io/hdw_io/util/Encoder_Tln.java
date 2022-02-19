2d7fbd243033a6e794240831a8046cd361d4a82a
JimHofm52 <JimHofmann52@gmail.com>
2022-02-18 21:58:26 -0500

    Copied in 2022 body for Encoder_Tln

--- src/main/java/frc/io/hdw_io/util/Encoder_Tln.java
+++ src/main/java/frc/io/hdw_io/util/Encoder_Tln.java
@@ -1,6 +1,5 @@
 package frc.io.hdw_io.util;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.math.util.Units;
@@ -15,24 +14,30 @@ public class Encoder_Tln {
         talonCtlr = escPort;
         tpf = _tpf;
     }
-  
+
+    /**@return Encoder ticks. */
     public double ticks(){
         return talonCtlr.getSelectedSensorPosition();
     }
 
+    /**@return calculated feet from ticks. */
     public double feet(){
         return tpf == 0.0 ? 0.0 : talonCtlr.getSelectedSensorPosition() / tpf;
     }
 
+    /**@return calcuate meters from feet. */
     public double meters() {
         return Units.feetToMeters(feet());
     }
 
+    /** Reset encoder count to zero. */
     public void reset(){
         talonCtlr.setSelectedSensorPosition(0, 0, 0);
     }
 
-    public double tpf() {
-        return tpf;
-    }
+    /**@return the existiing ticks per foot, tpf. */
+    public double getTPF() { return tpf; }
+
+    /**@param tpf - Set ticks per foot.  */
+    public void setTPF( double tpf) { this.tpf = tpf; }
 }
